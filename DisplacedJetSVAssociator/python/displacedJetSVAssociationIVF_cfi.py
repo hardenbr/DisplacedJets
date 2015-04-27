d110299b8774ed88c94e0c26d43372269fdf53c2
Joshua <joshua.hardenbrook@gmail.com>
2015-04-27 14:07:07 +0200

    adding displacedIVF fragment

---
+++ DisplacedJetSVAssociator/python/displacedJetSVAssociationIVF_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+displacedJetSVAssociationIVF = cms.EDProducer("DisplacedJetSVAssociator", 
+                                              caloJets          = cms.untracked.InputTag("ak4CaloJets","",""), 
+                                              secondaryVertices = cms.untracked.InputTag("displacedInclusiveSecondaryVertices"),
+                                              primaryVertices   = cms.untracked.InputTag("offlinePrimaryVertices"),
+                                              outputLabel       = cms.untracked.string('displacedIVFJetAssoc'),
+                                              jetPtCut          = cms.untracked.double(70.0),
+                                              alogname          = cms.untracked.string("oneOverDeltaR"),
+                                              debug             = cms.untracked.int32(1)
+)
